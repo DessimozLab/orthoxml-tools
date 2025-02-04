029a59d2e7fad268cc6374bb8891ab956ad4e5c3
Ali Yazdizadeh <ali@datachef.co>
2025-02-05 00:16:58 +0330

    feat: some initial code for loading

--- src/orthoxml/loaders.py
+++ src/orthoxml/loaders.py
@@ -1,5 +1,29 @@
 # loaders.py
+from lxml import etree
+from .exceptions import OrthoXMLParsingError
 
-def load_orthoxml_file(filepath):
-    # TODO
-    pass
+
+def load_orthoxml_file(filepath) -> etree.ElementTree:
+    """
+    Load an OrthoXML file from disk.
+    
+    :param filepath: Path to the OrthoXML file.
+    :return: An instance of the XML tree.
+    """
+    try:
+        return etree.parse(filepath)
+    except Exception as e:
+        raise OrthoXMLParsingError(f"Failed to load OrthoXML file: {e}")
+
+def parse_orthoxml(xml_tree) -> tuple:
+    """
+    Parse an OrthoXML document into genes, species, and groups.
+
+    :param xml_tree: An instance of the XML tree.
+    :return: A tuple of genes, species and groups.
+    """
+    genes = []
+    species = []
+    groups = []
+
+    return genes, species, groups
--- src/orthoxml/tree.py
+++ src/orthoxml/tree.py
@@ -1,22 +1,23 @@
 # tree.py
 
-from .loaders import load_orthoxml_file
+from .loaders import load_orthoxml_file, parse_orthoxml
 from .exceptions import OrthoXMLParsingError
 
 class OrthoXMLTree:
-    def __init__(self, species, groups, xml_tree):
+    def __init__(self, genes, species, groups, xml_tree):
+        self.genes = genes
         self.species = species
         self.groups = groups
         self.xml_tree = xml_tree
 
     def __repr__(self):
-        return f"<OrthoXMLTree: {len(self.species)} species, {len(self.groups)} groups>"
+        return f"<OrthoXMLTree: {len(self.genes)} genes, {len(self.species)} species, {len(self.groups)} groups>"
     
     def __str__(self):
         return f"OrthoXMLTree: {len(self.species)} species, {len(self.groups)} groups"
     
     @classmethod
-    def from_file(cls, filepath):
+    def from_file(cls, filepath) -> "OrthoXMLTree":
         """
         Factory method to load an OrthoXMLDocument from a file.
         
@@ -30,16 +31,12 @@ class OrthoXMLTree:
             raise OrthoXMLParsingError(f"Failed to load OrthoXML file: {e}")
         
         # Parse the XML tree into model objects.
-        species, groups = cls._parse_xml(xml_tree)
-        return cls(species, groups, xml_tree)
+        genes, species, groups = parse_orthoxml(xml_tree)
 
-    @staticmethod
-    def _parse_xml(xml_tree):
-        # TODO
-        return [], []
+        return cls(genes, species, groups, xml_tree)
 
     @classmethod
-    def from_string(cls, xml_data):
+    def from_string(cls, xml_str):
         pass
 
     def to_orthoxml(self, filepath=None, pretty=True):
