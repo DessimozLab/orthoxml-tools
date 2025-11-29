aeee535f176b265df6d5c739b0e65c6967e9d742
Ali Yazdizadeh <aliyzd1379@gmail.com>
2025-11-30 00:39:10 +0330

    temp: encode nhx taxonomy in orthoxml

--- src/orthoxml/converters/from_nhx.py
+++ src/orthoxml/converters/from_nhx.py
@@ -65,6 +65,24 @@ def nhx_species_encoded_leaf(node):
     return node.taxon.label, species
 
 
+def nhx_taxonomy_node_name(node):
+    """Extract taxonomy node name from NHX annotations.
+
+    Looks for T, S, or name annotations to identify taxonomy nodes.
+    Order of precedence: T -> S -> name
+
+    Returns None if no taxonomy annotation is found.
+    """
+    if not node.has_annotations:
+        return None
+
+    # Order of precedence: T -> S -> name
+    taxon_name = (node.annotations.get_value('T') or
+                  node.annotations.get_value('S') or
+                  node.annotations.get_value('name'))
+    return taxon_name
+
+
 class GeneRefHelper:
     def __init__(self, xml_root):
         self._xml_root = xml_root
@@ -115,6 +133,145 @@ class SpeciesConsecutiveGeneRefsHelper(GeneRefHelper):
         return str(self._generef_per_species[species])
 
 
+class TaxonomyBuilder:
+    """Helper class to build taxonomy trees from NHX annotations."""
+
+    def __init__(self):
+        self._taxon_id_counter = 0
+        self._taxon_name_to_id = {}
+        self._species_to_taxon_id = {}
+        self._internal_nodes = {}
+        self._taxonomy_tree = {}
+
+    def _next_taxon_id(self):
+        self._taxon_id_counter += 1
+        return self._taxon_id_counter
+
+    def _get_or_create_taxon_id(self, taxon_name):
+        """Get existing or create new taxon ID for given name."""
+        if taxon_name not in self._taxon_name_to_id:
+            self._taxon_name_to_id[taxon_name] = self._next_taxon_id()
+        return self._taxon_name_to_id[taxon_name]
+
+    def add_species(self, species_name):
+        """Add a species to the taxonomy."""
+        if species_name not in self._species_to_taxon_id:
+            taxon_id = self._get_or_create_taxon_id(species_name)
+            self._species_to_taxon_id[species_name] = taxon_id
+        return self._species_to_taxon_id[species_name]
+
+    def build_from_tree(self, tree, label_to_id_and_species):
+        """Build taxonomy structure from a dendropy tree."""
+
+        # First, collect all species from leaves
+        for leaf in tree.leaf_node_iter():
+            _, species = label_to_id_and_species(leaf)
+            self.add_species(species)
+
+        # Track species assignments to avoid circular references
+        species_assigned = set()
+
+        # Build internal node hierarchy
+        def process_node(node, parent_taxon_name=None):
+            if node.is_leaf():
+                return
+
+            # Get taxonomy name for this internal node
+            taxon_name = nhx_taxonomy_node_name(node)
+
+            if taxon_name:
+                taxon_id = self._get_or_create_taxon_id(taxon_name)
+                self._internal_nodes[taxon_name] = taxon_id
+
+                # Build parent-child relationships
+                if parent_taxon_name and parent_taxon_name != taxon_name:
+                    if parent_taxon_name not in self._taxonomy_tree:
+                        self._taxonomy_tree[parent_taxon_name] = []
+                    if taxon_name not in self._taxonomy_tree[parent_taxon_name]:
+                        self._taxonomy_tree[parent_taxon_name].append(taxon_name)
+
+                # Process children with this node as parent
+                for child in node.child_node_iter():
+                    if child.is_leaf():
+                        # Leaf nodes belong to this internal taxon
+                        _, species = label_to_id_and_species(child)
+                        species_key = f"{taxon_name}:{species}"
+                        if species_key not in species_assigned:
+                            if taxon_name not in self._taxonomy_tree:
+                                self._taxonomy_tree[taxon_name] = []
+                            if species not in self._taxonomy_tree[taxon_name]:
+                                self._taxonomy_tree[taxon_name].append(species)
+                            species_assigned.add(species_key)
+                    else:
+                        process_node(child, taxon_name)
+            else:
+                # No taxonomy name, process children with same parent
+                for child in node.child_node_iter():
+                    process_node(child, parent_taxon_name)
+
+        # Start from root
+        if tree.seed_node:
+            process_node(tree.seed_node)
+
+    def build_xml(self, parent_element):
+        """Build XML taxonomy section."""
+        if not self._taxonomy_tree and not self._internal_nodes:
+            return None
+
+        taxonomy_elem = ET.SubElement(parent_element, "taxonomy")
+
+        # Find root nodes (nodes that are not children of any other node)
+        all_children = set()
+        for parent, children in self._taxonomy_tree.items():
+            all_children.update(children)
+
+        root_nodes = []
+        for node in self._taxonomy_tree.keys():
+            if node not in all_children:
+                root_nodes.append(node)
+
+        # If no clear root structure, create an artificial root
+        if not root_nodes and self._taxonomy_tree:
+            # Create a root node that contains all top-level taxa
+            root_taxon_id = self._get_or_create_taxon_id("Root")
+            root_elem = ET.SubElement(taxonomy_elem, "taxon",
+                                    {"id": str(root_taxon_id), "name": "Root"})
+
+            # Add all taxa as children of root
+            for taxon_name in self._taxonomy_tree.keys():
+                self._build_taxon_element(root_elem, taxon_name)
+        else:
+            # Build tree from identified roots
+            for root_node in root_nodes:
+                self._build_taxon_element(taxonomy_elem, root_node)
+
+        return taxonomy_elem
+
+    def _build_taxon_element(self, parent_elem, taxon_name, visited=None):
+        """Recursively build taxon elements."""
+        if visited is None:
+            visited = set()
+
+        # Prevent infinite recursion from circular references
+        if taxon_name in visited:
+            return
+        visited.add(taxon_name)
+
+        taxon_id = self._taxon_name_to_id.get(taxon_name)
+        if not taxon_id:
+            # This is a species
+            taxon_id = self._species_to_taxon_id.get(taxon_name)
+
+        if taxon_id:
+            taxon_elem = ET.SubElement(parent_elem, "taxon",
+                                     {"id": str(taxon_id), "name": taxon_name})
+
+            # Add children if any
+            children = self._taxonomy_tree.get(taxon_name, [])
+            for child_name in children:
+                self._build_taxon_element(taxon_elem, child_name, visited.copy())
+
+
 class OrthoXMLBuilder:
     generef_helper = GeneRefHelper
 
@@ -128,8 +285,20 @@ class OrthoXMLBuilder:
         self.doc._setroot(self.xml)
         self._genehelper = self.generef_helper(self.xml)
         self._grpCnt = 0
+        self._taxonomy_builder = TaxonomyBuilder()
 
     def write(self, fh):
+        # Add taxonomy section after species and before groups
+        # Find the index of the groups element
+        groups_index = list(self.xml).index(self.grps)
+
+        # Build taxonomy and insert it before groups
+        taxonomy_element = self._taxonomy_builder.build_xml(self.xml)
+        if taxonomy_element is not None:
+            # Move taxonomy to correct position (before groups)
+            self.xml.remove(taxonomy_element)
+            self.xml.insert(groups_index, taxonomy_element)
+
         self.add_loft_ids()
         ET.indent(self.xml)
         self.doc.write(fh, xml_declaration=True, encoding="UTF-8", default_namespace=None)
@@ -168,6 +337,9 @@ class OrthoXMLBuilder:
     def add_group(self, tree, label_to_event, label_to_id_and_species):
         self._grpCnt += 1
         hogid = "HOG:{:07d}".format(self._grpCnt)
+
+        # Build taxonomy information from this tree
+        self._taxonomy_builder.build_from_tree(tree, label_to_id_and_species)
 
         for idx, node in enumerate(tree.preorder_node_iter(), start=1):
 
